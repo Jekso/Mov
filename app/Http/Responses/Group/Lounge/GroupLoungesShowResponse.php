9da6c9e85d8dca7401776846c694ebec31e723b6
Jekso.Senpai <jeksogsa@gmail.com>
2018-03-01 17:36:00 +0200

    Uni & Faculties, Complete Lounge Index, Start in Lounge Show 2

--- app/Http/Responses/Group/Lounge/GroupLoungesShowResponse.php
+++ app/Http/Responses/Group/Lounge/GroupLoungesShowResponse.php
@@ -27,6 +27,9 @@ class GroupLoungesShowResponse implements IResponsible
 	            'type' 			=> $this->lounge->type,
 	            'created_at' 	=> HelperGroupResponse::render_date($this->lounge->created_at),
 	            'updated_at' 	=> $this->lounge->updated_at,
+	            'user'			=> HelperGroupResponse::rednder_user($this->lounge->user).
+	            'images' 		=> $this->lounge->images->pluck('img'),
+                'poll_options'	=> $this->render_poll_option($lounge->poll_options)
 			]
 		];
 	}
